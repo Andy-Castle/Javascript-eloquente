5e9cda35cf56a5a76ca0af9ff3fcaa605360d2ef
Andy-Castle <masterchief_and@live.com>
2024-09-02 00:25:22 -0500

    sigue el reduce

---
+++ Capitulo 5 - Funciones de orden superior/Conjunto de datos de script.js
@@ -0,0 +1,9 @@
+{
+  name: "Copto",
+  rangos: [[994, 1008], [11392, 11508], [11513, 11520]],
+  dirección: "ltr",
+  año: -200,
+  vivo: false,
+  enlace: "https://es.wikipedia.org/wiki/Alfabeto_copto"
+}
+  
