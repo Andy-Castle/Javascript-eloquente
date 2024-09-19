577ef96d932d776a3c23a78c98720e0cfc9e93f0
Andy-Castle <masterchief_and@live.com>
2024-09-19 14:55:25 -0500

    las instrucciones del libro son poco claras

--- Capitulo 6 - La vida secreta de los objetos/Ejercicios.js
+++ Capitulo 6 - La vida secreta de los objetos/Ejercicios.js
@@ -45,3 +45,43 @@ console.log(new Vec(3, 4).length);
 
 /*Esto estaba sencillo, pero no sabia como crear una clase
 y yo mismo asignarle los valores, pero ya se como se hace.*/
+
+//Grupos
+
+class Group {
+  #members = [];
+
+  add(value) {
+    if (!this.has(value)) {
+      this.#members.push(value);
+    }
+  }
+
+  delete(value) {
+    this.#members = this.#members.filter((v) => v !== value);
+  }
+
+  has(value) {
+    return this.#members.includes(value);
+  }
+
+  static from(collection) {
+    let group = new Group();
+    for (let value of collection) {
+      group.add(value);
+    }
+    return group;
+  }
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+
+console.log(group.has(30));
+
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+
+//Esta esta interesante, pero las instrucciones del libro son poco claras
+//y tuve que ver la solución para entenderlo
